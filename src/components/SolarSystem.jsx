ed1751accc3d3e335969c02b19db36db4da9bad1
Larissa <larisssa_rga@hotmail.com>
2022-10-21 17:35:34 -0300

    Adiciona requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropType from 'prop-types';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
@@ -6,9 +9,22 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas"> </Title>
+        {planets.map(({ name, image }) => (
+          <PlanetCard
+            key={ name }
+            planetName={ name }
+            planetImage={ image }
+          />))}
       </div>
     );
   }
 }
+
+PlanetCard.propTypes = {
+  planet: PropType.shape({
+    planetName: PropType.string,
+    planetImage: PropType.string,
+  }).isRequired,
+};
 
 export default SolarSystem;
